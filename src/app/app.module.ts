3ebdcd9222200b0adf39b7ec15e3d267b4d0b362
RicharRomero123 <89022073+RicharRomero123@users.noreply.github.com>
2023-04-22 17:48:35 -0500

    create Componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomeComponent } from './home/home.component';
+import { PeliculasTableComponent } from './peliculas-table/peliculas-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ToolbarComponent,
+    HomeComponent,
+    PeliculasTableComponent
   ],
   imports: [
     BrowserModule,
