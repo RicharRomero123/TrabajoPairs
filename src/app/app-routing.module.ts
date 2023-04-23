14240a20253505ef6625d994e90957521a828a9f
FabrizCastro <fabrizziocastro2612@gmail.com>
2023-04-22 20:49:52 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'peliculas', component: PeliculasTableComponent },
+  {path:'business/peliculas' , component: PeliculasTableComponent}
 ];
 
 @NgModule({
